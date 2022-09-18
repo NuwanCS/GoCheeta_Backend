1765f77a26a4b2c84da109643df9c9f2a86cabca
Nuwan Senevirathne <smpg.nuwan@gmail.com>
2022-09-18 21:10:37 +0530

    Creating street and city model

---
+++ src/main/java/com/nuwan/gocheeta/model/Street.java
@@ -0,0 +1,52 @@
+package com.nuwan.gocheeta.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Nuwan
+ */
+public class Street extends BaseEntity{
+    private String name;
+
+    public Street() {
+    }
+
+    public Street(ResultSet rs)  throws SQLException {
+        this.id = rs.getLong("id");
+        this.name = rs.getString("name");
+        this.status =rs.getString("status");
+    }
+    public Street(Long id, String name, String status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
