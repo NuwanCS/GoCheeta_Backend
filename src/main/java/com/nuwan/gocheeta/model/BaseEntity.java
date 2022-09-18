dd560b823b06aedd5429ef40756d0a9437bbb813
Nuwan Senevirathne <smpg.nuwan@gmail.com>
2022-09-18 21:18:14 +0530

    Adding a baseEntityl

---
+++ src/main/java/com/nuwan/gocheeta/model/BaseEntity.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nuwan.gocheeta.model;
+
+/**
+ *
+ * @author Nuwan
+ */
+public class BaseEntity {
+    protected Long id;
+    protected String status;
+}
